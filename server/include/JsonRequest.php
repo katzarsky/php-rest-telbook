93129dc3ad071a90cb9ac98a06f2b79a668a363b
Boian Katzarsky <katzarsky@gmail.com>
2021-03-29 11:28:11 +0300

    Update JsonRequest.php

--- server/include/JsonRequest.php
+++ server/include/JsonRequest.php
@@ -4,13 +4,22 @@ class JsonRequest {
 	
 	public $method = '';
 	public $path = '';
+	public $body = null;
 	public $data = null;
 	
 	public function __construct() {
 		// get the HTTP method, path and body of the request
 		$this->method = $_SERVER['REQUEST_METHOD'];
 		$this->path = trim($_SERVER['PATH_INFO'],'/');
-		$this->data = json_decode(file_get_contents('php://input'));
+
+		if(in_array($this->method, ['POST','PUT','PATCH'])) {
+			$this->body = file_get_contents('php://input');
+			$content_type = $_SERVER["CONTENT_TYPE"] ?? null;
+
+			if($content_type == 'application/json') {
+				$this->data = json_decode($this->body);
+			}
+		}
 	}
 	
 	public function is($method, $route) {
