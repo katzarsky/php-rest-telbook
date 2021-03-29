1a431505a81da35f1454e50a9dd6fc30c50ea0b1
Boian Katzarsky <katzarsky@gmail.com>
2021-03-29 11:44:05 +0300

    Update router.php

--- server/router.php
+++ server/router.php
@@ -2,11 +2,11 @@
 
 // Route URL paths
 if($request->is('GET','persons')) {
-	$response->persons = $db->querybind_all('SELECT * FROM persons ORDER BY id');
+	$response->persons = $db->all('SELECT * FROM persons ORDER BY id');
 }
 else if($request->is('GET','persons/[0-9]+')) {
 	$person_id = (int) $request->segment(1);
-	$response->person = $db->querybind_one('SELECT * FROM persons WHERE id = ?', [ $person_id ]);
+	$response->person = $db->one('SELECT * FROM persons WHERE id = ?', [ $person_id ]);
 	if(!$response->person) {
 		$response->code(404);
 		$response->error('404: Person Not Found.');
@@ -30,34 +30,34 @@ else if($request->is('POST','persons/[0-9]+') || $request->is('POST','persons'))
 	}
 	else {
 		if($person_id > 0) { // update existing
-			$result = $db->querybind(
+			$result = $db->exec(
 				'UPDATE persons SET fname=?, lname=?, address=? WHERE id=?', 
 				[$person->fname, $person->lname, $person->address, $person_id]
 			);
 		} else { // insert new
-			$result = $db->querybind(
+			$result = $db->exec(
 				'INSERT INTO persons SET fname=?, lname=?, address=?', 
 				[$person->fname, $person->lname, $person->address]
 			);
 			$person_id = $db->insert_id;
 		}
 		
-		$response->person = $db->querybind_one('SELECT * FROM persons WHERE id = ?', [$person_id]);
+		$response->person = $db->one('SELECT * FROM persons WHERE id = ?', [$person_id]);
 		$response->info('Person saved.');	
 	}
 }
 else if($request->is('DELETE','persons/[0-9]+')) {
 	$person_id = (int) $request->segment(1);
-	$db->querybind('DELETE FROM telephones WHERE person_id = ?', [$person_id] );
-	$db->querybind('DELETE FROM persons WHERE id = ?', [$person_id] );
+	$db->exec('DELETE FROM telephones WHERE person_id = ?', [$person_id] );
+	$db->exec('DELETE FROM persons WHERE id = ?', [$person_id] );
 	$response->info("Person id=$person_id and its telephones deleted.");
 }
 else if($request->is('GET','persons/[0-9]+/telephones')) {
 	$person_id = (int) $request->segment(1);
-	$response->person = $db->querybind_one('SELECT * FROM persons WHERE id = ?', [$person_id] );
+	$response->person = $db->one('SELECT * FROM persons WHERE id = ?', [$person_id] );
 	$response->telephones = [];
 	if($response->person) {
-		$response->telephones = $db->querybind_all('SELECT * FROM telephones WHERE person_id = ?', [$person_id] );
+		$response->telephones = $db->all('SELECT * FROM telephones WHERE person_id = ?', [$person_id] );
 	}
 	else {
 		$response->code(404);
@@ -66,7 +66,7 @@ else if($request->is('GET','persons/[0-9]+/telephones')) {
 }
 else if($request->is('GET','telephones/[0-9]+')) {
 	$telephone_id = (int) $request->segment(1);
-	$response->telephone = $db->querybind_one('SELECT * FROM telephones WHERE id = ?', [ $telephone_id ]);
+	$response->telephone = $db->one('SELECT * FROM telephones WHERE id = ?', [ $telephone_id ]);
 	if(!$response->telephone) {
 		$response->code(404);
 		$response->error('404: Telephone Not Found.');
@@ -79,7 +79,7 @@ else if($request->is('POST','telephones/[0-9]+') || $request->is('POST','telepho
 		if(strlen($telephone->number) < 1) $response->error('Number is empty.');
 		if($telephone->person_id < 1) $response->error('Missing person_id.');
 		if($telephone->teltype_id < 1) $response->error('Type is empty.');
-//		$teltype = $db->querybind_one("SELECT * FROM teltypes WHERE id = ?", [$telephone->teltype_id + 0]);
+//		$teltype = $db->one("SELECT * FROM teltypes WHERE id = ?", [$telephone->teltype_id + 0]);
 //		if(!$teltype) $response->error('Type is invalid.');
 	}
 	else {
@@ -94,23 +94,23 @@ else if($request->is('POST','telephones/[0-9]+') || $request->is('POST','telepho
 		$args = [$telephone->person_id, $telephone->teltype_id, $telephone->number, $telephone_id];
 		
 		if($telephone_id > 0) { // update existing
-			$result = $db->querybind('UPDATE telephones SET person_id=?, teltype_id=?, number=? WHERE id=?', $args);
+			$result = $db->exec('UPDATE telephones SET person_id=?, teltype_id=?, number=? WHERE id=?', $args);
 		} else { // insert new
-			$result = $db->querybind('INSERT INTO telephones SET person_id=?, teltype_id=?, number=?', $args);
+			$result = $db->exec('INSERT INTO telephones SET person_id=?, teltype_id=?, number=?', $args);
 			$telephone_id = $db->insert_id;
 		}
 
-		$response->telephone = $db->querybind_one('SELECT * FROM telephones WHERE id = ?', [$telephone_id]);
+		$response->telephone = $db->one('SELECT * FROM telephones WHERE id = ?', [$telephone_id]);
 		$response->info('Telephone saved.');	
 	}
 }
 else if($request->is('DELETE','telephones/[0-9]+')) {
 	$telephone_id = (int) $request->segment(1);
-	$db->querybind('DELETE FROM telephones WHERE id = ?', [$telephone_id] );
+	$db->exec('DELETE FROM telephones WHERE id = ?', [$telephone_id] );
 	$response->info("Telephone id=$telephone_id deleted.");
 }
 else if($request->is('GET','teltypes')) {
-	$response->teltypes = $db->querybind_all('SELECT * FROM teltypes ORDER BY id');
+	$response->teltypes = $db->all('SELECT * FROM teltypes ORDER BY id');
 }
 else {
 	$response->error('404: URL Not Found: /'.$request->path);
